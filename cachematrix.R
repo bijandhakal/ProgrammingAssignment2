b5eb649dcd57d67c607261a3130d482a91cae1df
bijandakal <dhakal.bijan@gmail.com>
2015-09-09 19:43:40 +0545

    commiting first R file

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,26 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) { 
+  inverse_matrix<-NULL
+  set<-function(matrix){
+    x<<-matrix
+    inverse_matrix<<-NULL
+  }
+  get<-function() x
+  setReverse<-function(inverse) inverse_matrix <<-inverse
+  getReverse<-function() inverse_matrix
+  
+  list(set=set,get=get,setInverse=setInverse,getInverse=getInverse)
 }
-
-
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inverse_matrix<-x$getInverse()
+  if(!is.null(inverse_matrix)){
+    message("getting cached data")
+    return(inverse_matrix)
+  }
+  matrix <- x$get()
+  inverse_matrix <- solve(matrix)
+  x$setInverse(inverse_matrix)
+  inverse_matrix
 }
